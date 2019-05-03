cc457beba461527666a996769d67313972b353a3
Admin <dominik.tulak@email.cz>
2019-05-03 23:24:07 +0200

    Funkční Telnet ve Form1

--- CiscoCLIGuide/Form1.cs
+++ CiscoCLIGuide/Form1.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using PrimS.Telnet;
 
 namespace CiscoCLIGuide
 {
     public partial class Form1 : Form
     {
+        public Client tl;
+        public async Task tn()
+        {
+           
+            string outf = await tl.ReadAsync();
+            textBox1.Text += "\n" + outf;
+        }
         public Form1()
         {
             InitializeComponent();
-            Model.TelnetTest.Telnet(textBox1);
+            tl = new Client("tulak.host", 52556, new System.Threading.CancellationToken());
+            tn();
+            // client.WriteLine("show statistic wan2");
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                tl.WriteLine((sender as TextBox).Text);
+                (sender as TextBox).Text = "";
+                tn();
+            }
         }
     }
 }
--- CiscoCLIGuide/Model/TelnetTest.cs
+++ CiscoCLIGuide/Model/TelnetTest.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace CiscoCLIGuide.Model
 {
@@ -17,16 +18,21 @@ namespace CiscoCLIGuide.Model
     class TelnetTest
     {
         public static void Telnet(TextBox tb) {
-            TcpClient client = new TcpClient("tulak.host", 52556);
+            TcpClient client = new TcpClient("google.com", 23);
             StreamReader sr = new StreamReader(client.GetStream());
             StreamWriter sw = new StreamWriter(client.GetStream());
             string data = sr.ReadLine();
             while (data != null)
             {
                 tb.Text += "\n" + data;
+                MessageBox.Show(data);
+                sw.WriteLine("aaa");
                 data = sr.ReadLine();
+
             }
+            
             client.Close();
+            
         }
         
     }
