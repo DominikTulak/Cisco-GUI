15609b091291a73c5968c16dbda1161de8c90146
Admin <dominik.tulak@email.cz>
2019-05-04 22:39:17 +0200

    Přidán Telnet, okno konzole

---
+++ CiscoCLIGuide/Model/TelnetNastaveni.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace CiscoCLIGuide.Model
+{
+    /*
+     * Třída pro nastavení parametrů Telnetu
+     */ 
+    public static class TelnetNastaveni
+    {
+        //Adresa připojení
+        public static IPAddress adresaTelnetu = null;
+
+        //Port (výchozí 23)
+        public static int cisloPortu = 23;
+    }
+}
---
+++ CiscoCLIGuide/View/oknoNastaveniTelnetu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using CiscoCLIGuide.Model;
+
+namespace CiscoCLIGuide.View
+{
+    public partial class oknoNastaveniTelnetu : Form
+    {
+        public oknoNastaveniTelnetu()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUlozit_Click(object sender, EventArgs e)
+        {
+            //Převod textu na IP
+            IPAddress adresaTelnetu = null;
+            bool vysledekPrevodu = IPAddress.TryParse(textBoxIPAdresa.Text, out adresaTelnetu);
+
+            //Převod se nezdařil...
+            if (vysledekPrevodu == false)
+            {
+                MessageBox.Show("Neplatný formát adresy. Použij standartní IPv4 notaci!", "CHYBA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else //Převod se zdařil
+            {
+                //Uložíme hodnoty do příslušené třídy
+                TelnetNastaveni.adresaTelnetu = adresaTelnetu;
+                TelnetNastaveni.cisloPortu = (int)ciselnikPort.Value;
+            }
+        }
+
+        //Načtení okna (převyplnění adresy)
+        private void oknoNastaveniTelnetu_Load(object sender, EventArgs e)
+        {
+            textBoxIPAdresa.Text = Convert.ToString(Dns.GetHostByName(Dns.GetHostName()).AddressList[0]);
+        }
+    }
+}
